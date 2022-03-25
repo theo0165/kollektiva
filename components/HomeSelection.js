f5d1eca25ea4ab1cba23d9a30d6d57d8a75762cf
agnesskonvall <89479983+agnesskonvall@users.noreply.github.com>
2022-03-25 14:31:48 +0100

    added steps to signup form

--- components/HomeSelection.js
+++ components/HomeSelection.js
@@ -1,1 +1,4 @@
 // step 3, choose what kind of home you want to rent out (house, cabin etc)
+export default function HomeSelection() {
+  return <h1>Home</h1>;
+}
--- components/PartSelection.js
+++ components/PartSelection.js
@@ -1,1 +1,4 @@
 //step 2, choose if you want to rent out the entire house or just part of it
+export default function PartSelection() {
+  return <h1>PartSelection</h1>;
+}
