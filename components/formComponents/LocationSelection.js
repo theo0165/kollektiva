14141652a74ad775f8758356fa6d6a4652dab8d3
Theo Sandell <theo01sandell@gmail.com>
2022-03-29 15:02:14 +0200

    Default map now works

--- components/formComponents/LocationSelection.js
+++ components/formComponents/LocationSelection.js
@@ -1,5 +1,9 @@
+import dynamic from "next/dynamic";
+
 //step 5 choose where in the country the rental is located
 export default function LocationSelection() {
+  const Map = dynamic(() => import("../Map"), { ssr: false });
+
   return (
     <>
       <h1>Vart finns bostaden</h1>
@@ -7,6 +11,7 @@ export default function LocationSelection() {
         <label htmlFor="address">Sök efter din address</label>
         <input type="text" placeholder="Gatan 1, 123 45 Staden"></input>
       </div>
+      <Map />
     </>
   );
 }
