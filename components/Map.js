8b599d7a60f1110d4f8acc5faf17756e48b4b65f
Theo Sandell <theo01sandell@gmail.com>
2022-03-30 13:39:17 +0200

    Embed map now working

--- components/Map.js
+++ components/Map.js
@@ -3,32 +3,54 @@ import { Icon } from "leaflet";
 import "leaflet/dist/leaflet.css";
 import markerSvg from "../assets/marker.svg";
 
-export default function Map({ latitude, longitude }) {
-  const hasChoosenAddress = latitude != null && longitude != null;
-  const defaultLat = 63.0331902;
-  const defaultLon = 18.0652439;
+// export default function Map({ latitude, longitude }) {
+//   const hasChoosenAddress = latitude != null && longitude != null;
+//   const defaultLat = 63.0331902;
+//   const defaultLon = 18.0652439;
 
-  const markerIcon = new Icon({
-    iconUrl: markerSvg.src,
-    iconSize: [32, 32],
-  });
+//   const markerIcon = new Icon({
+//     iconUrl: markerSvg.src,
+//     iconSize: [32, 32],
+//   });
+
+//   return (
+//     <>
+//       {console.log("in component", latitude, longitude)}
+//       <MapContainer
+//         center={[latitude ?? defaultLat, longitude ?? defaultLon]}
+//         zoom={hasChoosenAddress ? 13 : 5}
+//         style={{ width: "100%", height: "500px" }}
+//       >
+//         <TileLayer
+//           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+//           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//         />
+//         {hasChoosenAddress && (
+//           <Marker position={[latitude, longitude]} icon={markerIcon} />
+//         )}
+//       </MapContainer>
+//     </>
+//   );
+// }
+
+export default function Map({ address }) {
+  const defaultLocation = "Sweden";
+
+  if (address != null && !address.toLowerCase().endsWith("sweden")) {
+    address = address + ", Sweden";
+  }
 
   return (
     <>
-      {console.log("in component", latitude, longitude)}
-      <MapContainer
-        center={[latitude ?? defaultLat, longitude ?? defaultLon]}
-        zoom={hasChoosenAddress ? 13 : 5}
-        style={{ width: "100%", height: "500px" }}
-      >
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {hasChoosenAddress && (
-          <Marker position={[latitude, longitude]} icon={markerIcon} />
-        )}
-      </MapContainer>
+      <iframe
+        width="100%"
+        height="500"
+        loading="lazy"
+        style={{ border: 0 }}
+        src={`https://www.google.com/maps/embed/v1/place?key=${
+          process.env.NEXT_PUBLIC_GOOGLE_API_KEY
+        }&q=${encodeURIComponent(address ?? defaultLocation)},+Sweden`}
+      ></iframe>
     </>
   );
 }
--- components/formComponents/LocationSelection.js
+++ components/formComponents/LocationSelection.js
@@ -3,28 +3,22 @@ import { useState } from "react";
 
 //step 5 choose where in the country the rental is located
 export default function LocationSelection() {
-  const [coords, setCoords] = useState([null, null]);
+  const [address, setAddress] = useState(null);
   const Map = dynamic(() => import("../Map"), { ssr: false });
 
   const handleInputChange = async (e) => {
     const value = e.target.value;
 
-    if (value === null || value === "") {
-      setCoords([null, null]);
+    if (value.length < 10) {
       return;
     }
 
-    const addressLookup = await fetch(
-      `/api/address_lookup?q=${decodeURIComponent(value)}`
-    );
-    const lookupResult = await addressLookup.json();
-
-    console.log(lookupResult);
-
-    if (!lookupResult.error) {
-      setCoords([lookupResult.latitude, lookupResult.longitude]);
-      console.log("Setting coords", coords);
+    if (value === null || value === "") {
+      setAddress(null);
+      return;
     }
+
+    setAddress(value);
   };
 
   return (
@@ -35,10 +29,10 @@ export default function LocationSelection() {
         <input
           type="text"
           placeholder="Gatan 1, 123 45 Staden"
-          onBlur={(e) => handleInputChange(e)}
+          onChange={(e) => handleInputChange(e)}
         ></input>
       </div>
-      <Map latitude={coords[0]} longitude={coords[1]} />
+      <Map address={address} />
     </>
   );
 }
