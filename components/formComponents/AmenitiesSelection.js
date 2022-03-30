e8f617e27c931c31eeae91a8dc2a952585ef07b8
agnesskonvall <89479983+agnesskonvall@users.noreply.github.com>
2022-03-30 13:27:24 +0200

    added structure to amenities and time-selection

--- components/formComponents/AmenitiesSelection.js
+++ components/formComponents/AmenitiesSelection.js
@@ -5,14 +5,38 @@ export default function AmenitiesSelection() {
       <h1>Vilka bekvämligheter finns i bostaden?</h1>
       <p>Välj alla alternativ som stämmer.</p>
       <div className="checkbox-buttons">
-        <input type="checkbox" id="heatingOne" name="heat" value="central" />
-        <label htmlFor="heatingOne">Centralvärme</label>
-        <input type="checkbox" id="heatingTwo" name="heat" value="electric" />
-        <label htmlFor="heatingTwo">Eluppvärmning</label>
-        <input type="checkbox" id="heatingThree" name="heat" value="gas" />
-        <label htmlFor="heatingThree">Gasvärme</label>
-        <input type="checkbox" id="heatingFour" name="heat" value="radiator" />
-        <label htmlFor="heatingFour">Radiatorvärme</label>
+        <input type="checkbox" id="balcony" name="balcony" value="balcony" />
+        <label htmlFor="balcony">Balkong/Uteplats</label>
+        <input type="checkbox" id="elevator" name="elevator" value="elevator" />
+        <label htmlFor="elevator">Hiss</label>
+        <input type="checkbox" id="air" name="air" value="air" />
+        <label htmlFor="air">Luftkonditionering</label>
+        <input
+          type="checkbox"
+          id="dishWasher"
+          name="dishWasher"
+          value="dishWasher"
+        />
+        <label htmlFor="dishWasher">Diskmaskin</label>
+        <input
+          type="checkbox"
+          id="washingMachine"
+          name="washingMachine"
+          value="washingMachine"
+        />
+        <label htmlFor="washingMachine">Tvättmaskin</label>
+        <input type="checkbox" id="dryer" name="dryer" value="dryer" />
+        <label htmlFor="dryer">Torktumlare</label>
+        <input type="checkbox" id="bathTub" name="bathTub" value="bathTub" />
+        <label htmlFor="bathTub">Badkar</label>
+        <input type="checkbox" id="garage" name="garage" value="garage" />
+        <label htmlFor="garage">Garage</label>
+        <input type="checkbox" id="parking" name="parking" value="parking" />
+        <label htmlFor="parking">Parkeringsplats</label>
+        <input type="checkbox" id="furnace" name="furnace" value="furnace" />
+        <label htmlFor="furnace">Kakelugn/Öppen spis</label>
+        <input type="checkbox" id="internet" name="internet" value="internet" />
+        <label htmlFor="internet">Internet</label>
       </div>
     </div>
   );
--- components/formComponents/TimeSelection.js
+++ components/formComponents/TimeSelection.js
@@ -1,1 +1,18 @@
 //step 6 choose the amount of time you wish to rent out your house
+export default function TimeSelection() {
+  return (
+    <div>
+      <h1>Hur länge vill du hyra ut bostaden?</h1>
+      <div className="date-inputs">
+        <input type="date" id="timeStart" name="timeStart" value="timeStart" />
+        <label htmlFor="timeStart">Hyr ut från:</label>
+        <input type="date" id="timeEnd" name="timeEnd" value="timeEnd" />
+        <label htmlFor="timeEnd">Hyr ut till:</label>
+      </div>
+      <h3>Uhyrningsperiod: </h3>
+      <p>
+        Från {timeStart} till {timeEnd}
+      </p>
+    </div>
+  );
+}
