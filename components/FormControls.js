8bb17134ea7f094c197f46c49a438d2ce26d85c0
Theo Sandell <theo01sandell@gmail.com>
2022-04-06 14:30:37 +0200

    Add form controls (back/forward)

--- components/FormControls.js
+++ components/FormControls.js
@@ -1,27 +1,51 @@
 import { useRouter } from "next/router";
+import styles from "../styles/FormControls.module.scss";
 
 const FormControls = ({ maxSteps, step, nextStep, prevStep }) => {
   const router = useRouter();
 
+  const stepDots = [];
+
+  for (let i = 0; i < maxSteps - 1; i++) {
+    stepDots.push(
+      <div
+        className={`${styles.dot} ${i < step - 1 ? styles.checked : ""}`}
+      ></div>
+    );
+  }
+
   return (
-    <div>
-      <div>
+    <div className={styles.container}>
+      {step > 1 && (
+        <div>
+          <p className={styles.stepCount}>
+            Steg {step - 1} av {maxSteps - 1}
+          </p>
+          <div className={styles.dotContainer}>{stepDots}</div>
+        </div>
+      )}
+
+      <div className={styles.btnContainer + " col-10"}>
         {step - 1 < 1 ? (
           <button
             onClick={() => {
               router.push("/");
-              console.log("hej");
             }}
+            className="btn"
           >
             Avbryt
           </button>
         ) : (
-          <button onClick={prevStep}>Bakåt</button>
+          <button onClick={prevStep} className="btn">
+            Bakåt
+          </button>
         )}
         {step + 1 > maxSteps ? (
-          <button>Publicera</button>
+          <button className="btn btn-success">Publicera</button>
         ) : (
-          <button onClick={nextStep}>Nästa</button>
+          <button onClick={nextStep} className="btn btn-primary">
+            Nästa
+          </button>
         )}
       </div>
     </div>
--- components/formComponents/Success.js
+++ components/formComponents/Success.js
@@ -1,1 +1,4 @@
 // 13 and final step, your rental is now published
+export default function Success() {
+  return <div>Success</div>;
+}
