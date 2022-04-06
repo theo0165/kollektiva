633bf80be478fd195a28bba950ca5c28e6dbc9e2
Theo Sandell <theo01sandell@gmail.com>
2022-04-06 13:10:23 +0200

    Added link in header

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,14 @@
 import logo from "../assets/logo.svg";
 import { MdMenu } from "react-icons/md";
 import styles from "../styles/Header.module.scss";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header className={styles.container}>
-      <img src={logo.src} />
+      <Link href="/">
+        <img src={logo.src} />
+      </Link>
       <MdMenu size="40px" color="#23449b" />
     </header>
   );
