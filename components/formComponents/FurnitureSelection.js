b222e8afb9d4e0fec3924fe28d5818b544cde162
agnesskonvall <89479983+agnesskonvall@users.noreply.github.com>
2022-04-07 14:16:03 +0200

    added styling to furniture-selection

--- components/formComponents/FurnitureSelection.js
+++ components/formComponents/FurnitureSelection.js
@@ -2,43 +2,48 @@
 
 export default function FurnitureSelection({ state, handleChange }) {
   return (
-    <div>
+    <div className="col-10">
       <h1 className="form-title">Hur hyrs bostaden ut?</h1>
       <p className="form-subtitle">Välj ett alternativ.</p>
-      <div className="radio-buttons">
-        <input
-          type="radio"
-          id="fullyFurnished"
-          name="furniture"
-          value="fullyFurnished"
-          onChange={handleChange("furniture")}
-          checked={state.furniture === "fullyFurnished" ? true : false}
-        />
-        <label className="form-label" htmlFor="fullyFurnished">
-          Möblerad
-        </label>
-        <input
-          type="radio"
-          id="partiallyFurnished"
-          name="furniture"
-          value="partiallyFurnished"
-          onChange={handleChange("furniture")}
-          checked={state.furniture === "partiallyFurnished" ? true : false}
-        />
-        <label className="form-label" htmlFor="partiallyFurnished">
-          Delvis möblerad
-        </label>
-        <input
-          type="radio"
-          id="unFurnished"
-          name="furniture"
-          value="unFurnished"
-          onChange={handleChange("furniture")}
-          checked={state.furniture === "unFurnished" ? true : false}
-        />
-        <label className="form-label" htmlFor="unFurnished">
-          Omöblerad
-        </label>
+      <div className="input-container">
+        <div className="radio-buttons">
+          <input
+            type="radio"
+            className="form-radio"
+            id="fullyFurnished"
+            name="furniture"
+            value="fullyFurnished"
+            onChange={handleChange("furniture")}
+            checked={state.furniture === "fullyFurnished" ? true : false}
+          />
+          <label className="form-label" htmlFor="fullyFurnished">
+            Möblerad
+          </label>
+          <input
+            type="radio"
+            className="form-radio"
+            id="partiallyFurnished"
+            name="furniture"
+            value="partiallyFurnished"
+            onChange={handleChange("furniture")}
+            checked={state.furniture === "partiallyFurnished" ? true : false}
+          />
+          <label className="form-label" htmlFor="partiallyFurnished">
+            Delvis möblerad
+          </label>
+          <input
+            type="radio"
+            className="form-radio"
+            id="unFurnished"
+            name="furniture"
+            value="unFurnished"
+            onChange={handleChange("furniture")}
+            checked={state.furniture === "unFurnished" ? true : false}
+          />
+          <label className="form-label" htmlFor="unFurnished">
+            Omöblerad
+          </label>
+        </div>
       </div>
     </div>
   );
