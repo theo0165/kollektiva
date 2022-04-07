1c4d16e1e8d9c71a125a3ae86e0caf8ecd9a8167
Theo Sandell <theo01sandell@gmail.com>
2022-04-07 13:24:35 +0200

    Add for radio class

--- components/formComponents/PartSelection.js
+++ components/formComponents/PartSelection.js
@@ -1,29 +1,35 @@
 //step 2, choose if you want to rent out the entire house or just part of it
 export default function PartSelection({ state, handleChange }) {
   return (
-    <div>
+    <div className="col-10">
       <h1 className="form-title">Jag vill hyra ut: </h1>
       <p className="form-subtitle">Välj ett alternativ.</p>
       <div className="radio-buttons">
-        <input
-          type="radio"
-          id="partOne"
-          name="rent"
-          value="whole"
-          onChange={handleChange("rent")}
-          checked={state.rent === "whole" ? true : false}
-        />
-        <label htmlFor="partOne">Hela min bostad</label>
+        <div>
+          <input
+            type="radio"
+            id="partOne"
+            name="rent"
+            value="whole"
+            onChange={handleChange("rent")}
+            checked={state.rent === "whole" ? true : false}
+            className="form-radio"
+          />
+          <label htmlFor="partOne">Hela min bostad</label>
+        </div>
         <p>Eller</p>
-        <input
-          type="radio"
-          id="partTwo"
-          name="rent"
-          value="part"
-          onChange={handleChange("rent")}
-          checked={state.rent === "part" ? true : false}
-        />
-        <label htmlFor="partTwo">En del av min bostad</label>
+        <div>
+          <input
+            type="radio"
+            id="partTwo"
+            name="rent"
+            value="part"
+            onChange={handleChange("rent")}
+            checked={state.rent === "part" ? true : false}
+            className="form-radio"
+          />
+          <label htmlFor="partTwo">En del av min bostad</label>
+        </div>
       </div>
     </div>
   );
