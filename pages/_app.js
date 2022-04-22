05aaac212c0949f473c6c042530e8bfd148a1627
Theo Sandell <theo01sandell@gmail.com>
2022-04-22 09:45:41 +0200

    Title

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,16 @@ import "../styles/globals.scss";
 import "react-alice-carousel/lib/scss/alice-carousel.scss";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   const getLayout =
     Component.getLayout ||
     ((page) => (
       <>
+        <Head>
+          <title>Kollektiva</title>
+        </Head>
         <Header />
         {page}
         <Footer />
